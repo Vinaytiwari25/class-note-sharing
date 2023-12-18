cb5679c21145ad563fddee694f41429606039f66
Vinay Tiwari <vt39091@gmail.com>
2023-12-18 21:39:08 +0530

    Defined relationships in Note and Category models

--- app/Models/Note.php
+++ app/Models/Note.php
@@ -8,4 +8,22 @@ use Illuminate\Database\Eloquent\Model;
 class Note extends Model
 {
     use HasFactory;
+
+    // Define the relationship with the Category model
+    public function category()
+    {
+        return $this->belongsTo(Category::class);
+    }
+
+    // Define the relationship with the Comment model
+    public function comments()
+    {
+        return $this->hasMany(Comment::class);
+    }
+
+    // Define the relationship with the Rating model
+    public function ratings()
+    {
+        return $this->hasMany(Rating::class);
+    }
 }
